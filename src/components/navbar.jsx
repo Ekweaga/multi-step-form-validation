060cc4cbef6c5e1dd2c6ce43acff9e76d7767e05
Ekweaga <hikay133@gmail.com>
2023-10-05 12:07:19 +0100

    Dependencies and navbar done

---
+++ src/components/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from "../images/Frame 34.png"
+function Navbar() {
+  return (
+    <>
+
+    <div>
+
+        <nav>
+            <div>
+                <img src={logo}/>
+            </div>
+
+
+            <div>
+                <ul></ul>
+            </div>
+
+            <div></div>
+        </nav>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
